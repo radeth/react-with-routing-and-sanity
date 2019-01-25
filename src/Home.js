a4bea10accb75c1d1244d6cde5c0294b0b14ffc7
radeth <33368809+radeth@users.noreply.github.com>
2019-01-25 14:57:56 +0100

    wow

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import sanityClient from "@sanity/client";
+const client = sanityClient({
+  projectId: "jslptlpf",
+  dataset: "production",
+  useCDN: true
+});
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    client.fetch('*[_type == "post"]').then(posts => this.setState({ posts }));
+  }
+
   render() {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <h1>Home</h1>
+        <ul>
+          {this.state.posts.map(post => (
+            <li key={post._id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
